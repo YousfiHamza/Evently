c429f593a0dbfb9513eec17e6c1bdb53432262be
YousfiHamza <yousfihamza22@gmail.com>
2024-01-03 10:00:04 +0100

    feat: implementing remaining models + Clerk webhook setups

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -12,6 +12,7 @@ export interface IEvent extends Document {
   price: string;
   isFree: boolean;
   url?: string;
+  category: { _id: string; name: string };
   organizer: { _id: string; firstName: string; lastName: string };
 }
 
@@ -26,6 +27,7 @@ const EventSchema = new Schema({
   price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
   organizer: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
